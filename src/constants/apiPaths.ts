65c2406a333cfe589fd0f74f8738aa399e731343
Raman Aleksandrou <raman.aleksandrou@gmail.com>
2026-06-19 20:10:58 +0200

    feat: point Cart Service to Elastic Beanstalk deployment URL

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://byb2npd55e.execute-api.eu-central-1.amazonaws.com/prod",
-  order: "https://r4habz0xwf.execute-api.eu-central-1.amazonaws.com/api/profile/cart",
+  order: "http://raman-aleksandrou-cart-api-prod.eu-central-1.elasticbeanstalk.com/api/profile/cart",
   import: "https://6g3uxcq0d6.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://r4habz0xwf.execute-api.eu-central-1.amazonaws.com/api",
+  cart: "http://raman-aleksandrou-cart-api-prod.eu-central-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
